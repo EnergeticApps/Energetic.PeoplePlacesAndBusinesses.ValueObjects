6e517e0a7ef6571c6585095149cd9b3ea9180e5e
Benjamin <benjaminjcharlton@gmail.com>
2021-03-31 17:29:05 +0400

    Created this beautiful value object for user IDs

---
+++ People/UserId.cs
@@ -0,0 +1,17 @@
+﻿using Energetic.ValueObjects;
+using Energetic.ValueObjects.JsonConverters;
+using Energetic.ValueObjects.TypeConverters;
+using System;
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace Energetic.People.ValueObjects
+{
+    [JsonConverter(typeof(WrappedGuidJsonConverter<UserId>))]
+    [TypeConverter(typeof(WrappedGuidTypeConverter<UserId>))]
+    public record UserId : WrappedGuidValueObject<UserId>
+    {
+        public UserId(Guid guid) : base(guid) { }
+        public UserId(string guid) : base(guid) { }
+    }
+}
